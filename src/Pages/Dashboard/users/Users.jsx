627435816d1c5ec8e4c20a46b82ae621f38f83f0
Sagarchakkarkota <chakkarkotasagar@gmail.com>
2023-12-07 19:16:53 +0530

    Add : Added Filters and Sorting in React table

--- src/Pages/Dashboard/users/Users.jsx
+++ src/Pages/Dashboard/users/Users.jsx
@@ -1,138 +1,9 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { getUsers } from "../../../lib/axios/apiServices/goRestQuery/goRestQuery";
-// import SideBar from "../../../components/sidebar/SideBar";
-
-// import { useState } from "react";
-
 // const Users = () => {
-//   const [pageNumber, setPageNumber] = useState(1);
-//   const pageSize = 5;
-
-//   const paginate = (data, pageNumber, pageSize) => {
-//     const startIndex = (pageNumber - 1) * pageSize;
-//     const endIndex = startIndex + pageSize;
-//     return data?.slice(startIndex, endIndex);
-//   };
-
-//   const { data, isLoading } = useQuery({
-//     queryKey: ["users"],
-//     queryFn: getUsers,
-//     staleTime: 10000,
-//   });
-
-//   const handleNext = () => {
-//     setPageNumber((prevPageNumber) => prevPageNumber + 1);
-//   };
-
-//   const handlePrev = () => {
-//     setPageNumber((prevPageNumber) => prevPageNumber - 1);
-//   };
-
-//   const paginatedData = paginate(data, pageNumber, pageSize);
 
 //   return (
-//     <div className="flex lg:h-screen  bg-gradient-to-br from-[#A5D8F3] to-[#F5D300] ">
-//       {/* <div className="flex "> */}
-//       <SideBar />
-//       <div>
-//         {isLoading ? (
-//           " ...Loading"
-//         ) : (
-//           <div className=" w-full p-4  ">
-//             {/* Table start.... */}
-//             <div className=" w-full">
-//               <h1 className="xlBoldFont text-center mb-4 md:mb-6">User Data</h1>
-
-//               <div className="w-full flex ">
-//                 {/* Table header start.... */}
-//                 <div className="tableBody ">Name</div>
-//                 <div className="tableBody ">User Name</div>
-//                 <div className="tableBody ">Mail Id</div>
-//                 <div className="tableBody ">Phone</div>
-//                 <div className="tableBody ">Website</div>
-//                 <div className="tableBody ">Address</div>
-//                 <div className="tableBody ">Company</div>
-//               </div>
-//               {/* Table header end.... */}
-
-//               {/* Table body start */}
-//               <div className="w-full flex flex-col xl:text-xs">
-//                 {paginatedData?.map(
-//                   ({
-//                     id,
-//                     name,
-//                     phone,
-//                     email,
-//                     username,
-//                     website,
-//                     address,
-//                     company,
-//                   }) => (
-//                     <div key={id} className="w-full flex flex-row">
-//                       <div className="tableBody">{name}</div>
-//                       <div className="tableBody">{username}</div>
-//                       <div className="tableBody flex-shrink break-all  ">
-//                         {email}
-//                       </div>
-//                       <div className="tableBody">{phone}</div>
-//                       <div className="tableBody">{website}</div>
-
-//                       <div className="tableBody flex-wrap ">
-//                         <div className="w-full flex flex-wrap">
-//                           {address.city}
-//                         </div>
-
-//                         <div className=" w-full flex flex-wrap">
-//                           {address.street}
-//                         </div>
-//                         <div className=" w-full flex flex-wrap">
-//                           {address.suite}
-//                         </div>
-//                         <div className=" w-full flex flex-wrap">
-//                           {address.zipcode}
-//                         </div>
-//                       </div>
-//                       <div className="tableBody flex-wrap">
-//                         <div className="w-full flex flex-wrap">
-//                           {company.bs}{" "}
-//                         </div>
-//                         <div className="w-full flex flex-wrap">
-//                           {company.name}{" "}
-//                         </div>
-//                       </div>
-//                     </div>
-//                   )
-//                 )}
-//               </div>
-
-//               {/* Table body end */}
-//             </div>
-//             {/* Table end.... */}
-//             {/* Next and Prev Button start...... */}
-//             <div className="w-full flex justify-center">
-//               {" "}
-//               <button
-//                 className="px-5 text-white"
-//                 onClick={handlePrev}
-//                 disabled={pageNumber === 1}
-//               >
-//                 Pev
-//               </button>
-//               <span className="text-white">
-//                 {pageNumber}/ {Math.ceil(data.length / pageSize)}
-//               </span>
-//               <button
-//                 className="px-5 text-white"
-//                 onClick={handleNext}
-//                 disabled={pageNumber === Math.ceil(data.length / pageSize)}
-//               >
-//                 Next
-//               </button>
-//             </div>
-//             {/* Next and Prev Button end...... */}
-//           </div>
-//         )}
-//       </div>
+//
+//
+//     </div>
 //     </div>
 //   );
 // };
@@ -145,10 +16,20 @@ import {
   getCoreRowModel,
   useReactTable,
   getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { getUsers } from "src/lib/axios/apiServices/goRestQuery/goRestQuery";
 import { HiDotsVertical } from "react-icons/hi";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import { GiSettingsKnobs } from "react-icons/gi";
+import { Popover, Transition } from "@headlessui/react";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import SideBar from "../../../components/sidebar/SideBar";
+
 const dataList = [
   {
     id: 1,
@@ -280,6 +161,9 @@ const Users = () => {
   //   queryFn: getUsers,
   //   staleTime: 10000,
   // });
+  const [sorting, setSorting] = useState([]);
+  const [filtering, setFiltering] = useState("");
+  const [columnVisibility, setColumnVisibility] = useState({});
 
   const tableColumns = [
     {
@@ -325,9 +209,9 @@ const Users = () => {
       accessorKey: "website",
       cell: (info) => <a href="https://www.google.co.in/">{info.getValue()}</a>,
     },
+
     {
       header: "Edit",
-      accessorKey: "button",
       cell: ({ row }) => (
         <button onClick={() => alert(JSON.stringify(row.original))}>
           <HiDotsVertical />
@@ -341,88 +225,170 @@ const Users = () => {
     columns: tableColumns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     initialState: {
       pagination: {
         pageSize: 5,
       },
     },
+    state: {
+      sorting: sorting,
+      globalFilter: filtering,
+      columnVisibility: columnVisibility,
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
+    onColumnVisibilityChange: setColumnVisibility,
   });
-  console.log(table.getPaginationRowModel());
+  const paginationState = table.getState();
+
   return (
-    <div className="w3-container">
-      <table className="w3-table w3-striped">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => {
-            return (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <th key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
+    <div className="flex lg:h-screen  bg-gradient-to-br  to-[#f1e8b1] w-full">
+      <div className="flex w-full ">
+        <SideBar />
+        <div className=" w-full p-6">
+          {/* SearchBar start */}
+          <div className="w-full flex justify-between items-center rounded-md border p-2 border-gray-200">
+            <input
+              type="text"
+              value={filtering}
+              onChange={(e) => {
+                <label htmlFor="">Search</label>;
+                setFiltering(e.target.value);
+              }}
+              placeholder="SearchBar"
+              className="border boder-gray-300  focus:outline-none "
+            />
+            <Popover>
+              {({ open }) => (
+                <>
+                  <Popover.Button>
+                    {" "}
+                    <div className="transform rotate-90">
+                      <GiSettingsKnobs />
+                    </div>
+                  </Popover.Button>
+
+                  {/* Use the `Transition` component. */}
+                  <Transition
+                    show={open}
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-75 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0"
+                    className="relative"
+                  >
+                    {/* Mark this component as `static` */}
+                    <Popover.Panel className="absolute z-10 bg-white top-[-4px] right-0 ">
+                      <div className="inline-block border border-rounded w-40 h-50 p-4">
+                        {table.getAllLeafColumns().map((column) => {
+                          return (
+                            <div
+                              key={column.id}
+                              className="px-1 border border-b-gray-200"
+                            >
+                              <input
+                                {...{
+                                  type: "checkbox",
+                                  checked: column.getIsVisible(),
+                                  onChange: column.getToggleVisibilityHandler(),
+                                }}
+                              />
+                              <label className="px-2">{column.id}</label>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </div>
+          {/* Table start */}
+          <table className="w-full border boder-gray-300 bg-gray-200 my-4 ">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => {
+                return (
+                  <tr key={headerGroup.id} className="border boder-gray-300 ">
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <th
+                          className="p-2 "
+                          key={header.id}
+                          onClick={header.column.getToggleSortingHandler()}
+                        >
+                          {header.isPlaceholder ? null : (
+                            <div className="flex flex-row   ">
+                              <div className="">
+                                {" "}
+                                {flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                                )}
+                              </div>
+                              <div className="p-1">
+                                {
+                                  {
+                                    asc: <IoIosArrowUp />,
+                                    desc: <IoIosArrowDown />,
+                                  }[header.column.getIsSorted() ?? null]
+                                }
+                              </div>
+                            </div>
                           )}
-                    </th>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </thead>
-        <tbody>
-          {table?.getRowModel().rows.map((row) => {
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      {/* <button
-        className="border border-black px-2 mx-1"
-        onClick={() => {
-          table.setPageIndex(0);
-        }}
-      >
-        firstpage
-      </button> */}
-      <button
-        className="border border-black px-2 mx-1"
-        onClick={() => {
-          table.previousPage();
-        }}
-      >
-        prev
-      </button>
-      {table.getPaginationRowModel}
-      <button
-        className="border border-black px-2 mx-1"
-        onClick={() => {
-          table.nextPage();
-        }}
-      >
-        next
-      </button>
-      {/* <button
-        className="border border-black px-2 mx-1"
-        onClick={() => {
-          table.setPageIndex(table.getPageCount() - 1);
-        }}
-      >
-        lastpage
-      </button> */}
+                        </th>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </thead>
+            <tbody className="text-sm">
+              {table?.getRowModel().rows.map((row) => {
+                return (
+                  <tr key={row.id} className="border boder-gray-300  ">
+                    {row.getVisibleCells().map((cell) => {
+                      return (
+                        <td key={cell.id} className="p-2 ">
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="w-full flex justify-end ">
+            <button
+              className="border border-black px-2 mx-1 rounded-sm "
+              disabled={!table.getCanPreviousPage()}
+              onClick={() => {
+                table.previousPage();
+              }}
+            >
+              <IoIosArrowRoundBack />
+            </button>
+            {paginationState.pagination.pageIndex + 1}/{table.getPageCount()}
+            <button
+              className="border border-black px-2 mx-1 rounded-sm"
+              disabled={!table.getCanNextPage()}
+              onClick={() => {
+                table.nextPage();
+              }}
+            >
+              <IoIosArrowRoundForward />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
